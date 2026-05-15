0055f7a0582d85a081a945dda7e0669a2c927685
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-15 10:25:27 +0300

    поместил код в функцию которую поместил в точку входа +[7-23] сделал исключение на "PermissionError" И "ValueError"

--- py_file/ls.py
+++ py_file/ls.py
@@ -5,6 +5,23 @@ init()  # или init(autoreset=True)
 
 # os.system('dir') # старый вывод папок и файлов
 
+# иницилизация colorama
+    init(autoreset=True) # или init()
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!! получение папок и файлов !!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    try:
+        list_files = [f for f in os.listdir(".") if os.path.isfile(os.path.join(".", f))]
+        list_files.sort()
+        list_dirs = [d for d in os.listdir(".") if os.path.isdir(os.path.join(".", d))]
+        list_dirs.sort()
+    except PermissionError:
+        print(Fore.LIGHTRED_EX + "command ls: Permission denied")# в доступе отказано
+        print(Fore.LIGHTGREEN_EX + "using command 'dir'")
+        os.system("dir")
+        sys.exit()
+    except ValueError:
+        sys.exit()
 
 list_files_and_dir = os.listdir()
 for file_or_dir in list_files_and_dir:
