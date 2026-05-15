b1ab4eeb33472b83261444834243ab778a5de8b4
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-15 11:12:47 +0300

    сделал условие проверяющее систему на которой запускается скрипт сделал вываод куда что отправлено при сортировке

--- py_file/sortfile.py
+++ py_file/sortfile.py
@@ -1,22 +1,29 @@
 import os
-import getpass
+from colorama import Fore
 
 
 print("сортировка файлов")
+if os.name == "nt":
+    USER_NAME = os.environ["USERNAME"] # запрашиваю userName
+    print(f"пользователь: {USER_NAME}")
+    os.chdir(f"C:\\Users\\{USER_NAME}\\Downloads") # перехожу в папку загрузок
+    path_to_directory_downloads = os.getcwd() # возвращаю полный путь до папки с загрузками
+    all_files = os.listdir()
+    print(f"путь до папки 'загрузки': {path_to_directory_downloads}")
 
-USER_NAME = getpass.getuser() # запрашиваю userName
-print(USER_NAME)
-os.chdir(f"C:\\Users\\{USER_NAME}\\Downloads") # перехожу в папку загрузок
-path_to_directory_downloads = os.getcwd() # возвращаю полный путь до папки с загрузками
-all_files = os.listdir()
-print(path_to_directory_downloads)
+    for file in all_files:
+        if file[-4:] in [".png", ".jpg", "jfif", "jpeg"]:
+            os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Pictures\\{file}")
+            print(f"{file} - отправлен в папку 'Pictures'")
+        if file[-4:] in [".gif", ".mp4"]:
+            os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Videos\\{file}")
+            print(f"{file} - отправлен в папку 'Videos'")
+        if file[-4:] in [".wav", ".mp3", ".mp2"]:
+            os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Music\\{file}")
+            print(f"{file} - отправлен в папку 'Music'")
+    else:
+        print(Fore.LIGHTGREEN_EX + "работа завершена!!!")
 
-for file in all_files:
-    print(file)
-    if file[-4:] in [".png", ".jpg", "jfif"]:
-        os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Pictures\\{file}")
-    if file[-4:] in [".gif", ".mp4"]:
-        os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Videos\\{file}")
-    if file[-4:] in [".wav", ".mp3", ".mp2"]:
-        os.replace(f"{path_to_directory_downloads}\\{file}", f"C:\\Users\\{USER_NAME}\\Music\\{file}")
-
+if "posix" == "posix":
+     USER_NAME = os.environ["USERNAME"]
+     print(USER_NAME)
